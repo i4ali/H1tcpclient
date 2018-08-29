912ebc433d601fce2f1b272427c1b789369f7864
Imran Ali <ImranA@coban.local>
2018-08-29 16:37:55 -0500

    removed threading(not needed) fully working solution

--- auto_record_upload.py
+++ auto_record_upload.py
@@ -6,7 +6,7 @@ import custom_logger as cl
 import logging
 import json
 
-disconnect = False
+retries = 20 # the number of times to poll H1 for response before timeout
 
 def add_delay_before(secs):
     def real_add_delay(func_to_decorate):
@@ -22,35 +22,63 @@ def record_videos(camera, rec_length_secs, quantity):
     # loop for number of videos specified
     while quantity>0:
         # send record command for camera number
-        h1.sendjson({"command":"record","camera":camera})
+        start_record({"command":"record","camera":camera})
         # send stop command after time specified
         time.sleep(rec_length_secs)
-        h1.sendjson({"command":"stoprecord","camera":camera})
+        stop_record({"command":"stoprecord","camera":camera})
         quantity = quantity - 1
         time.sleep(5) # add estimated delay of 5seconds b/w videos
 
 @add_delay_before(10)
 def start_upload():
-    h1.sendjson({"command":"upload", "icv":True})
+    resp = send_command_to_H1({"command":"upload", "icv":True})
+    if resp == -1:
+        raise IOError("Unable to start upload")
+    elif resp == 0:
+        log.info("upload start was successfull")
+
+def connect(ip,port):
+    h1.connect(ip,port)
+    resp = send_command_to_H1({"command":"ping"})
+    if resp == -1:
+        raise IOError("Unable to connect to H1")
+    elif resp == 0:
+        log.info("connection to {0} on port {1} successful".format(ip,port))
+
+def start_record(cmd):
+    resp = send_command_to_H1(cmd)
+    if resp == -1:
+        log.warning("Unable to start recording on camera {0}".format(cmd['camera']))
+    elif resp == 0:
+        log.info("recording started on camera {0}".format(cmd['camera']))
+
+def stop_record(cmd):
+    resp = send_command_to_H1(cmd)
+    if resp == -1:
+        log.warning("Unable to stop recording on camera {0}".format(cmd['camera']))
+    elif resp == 0:
+        log.info("recording stopped on camera {0}".format(cmd['camera']))
 
 
-def print_data_from_H1():
-    global disconnect
-    PrevResponse = ''
-    while not disconnect:
+def send_command_to_H1(cmd):
+    h1.sendjson(cmd)  
+    resp = get_response_from_H1(cmd)
+    return resp
+
+
+def get_response_from_H1(cmd):
+    global retries
+    while retries>0:
         h1.poll()
         response = h1.message
-        if (response != '') and (response != PrevResponse):
-            #if status is 0 say success for command else say failed
-            try:
-                if (json.loads(response[4:])['status']) == 0:
-                    log.info("{0} successfull".format(json.loads(response[4:])['command']))
-                else:
-                    log.info("{0} failed".format(json.loads(response[4:])['command']))
-            except Exception as e:
-                log.warning("unable to get JSON object because of {0}".format(str(e)))
-            PrevResponse = response.strip()
-            time.sleep(0.1)
+        if (response != ''):
+            if (json.loads(response[4:])['status']) == 0 and (json.loads(response[4:])['command']) == cmd['command']:
+                return 0
+            elif (json.loads(response[4:])['status']) != 0 and (json.loads(response[4:])['command']) == cmd['command']:
+                return -1
+        retries = retries-1
+        time.sleep(0.1)
+    return -1 
 
 
 if __name__ == '__main__':
@@ -70,27 +98,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    
-    # a seperate thread to handle receiving data from H1
-    t = threading.Thread(target=print_data_from_H1)
-
-    # instantiate connection to H1
     h1 = H1()
-    log.debug("connecting to H1 using IP {0} and Port {1}".format(args.ip, args.port))
-    h1.connect(args.ip, args.port)
-    h1.sendjson({"command":"ping"})
-
-    # start the new thread
-    log.debug("starting thread to listen for messages from H1")
-    t.start()
+    
+    # connect to H1
+    connect(args.ip,args.port)
 
     # record videos with camera number, length and number of videos specified
-    log.debug("starting recording {0} videos from camera {1} for {2} secs each".format(args.videos, args.camera, args.recordingtime ))
     record_videos(args.camera, args.recordingtime, args.videos)
 
     # upload if specified  
     if args.upload:
-        log.debug("starting upload")
         start_upload()
 
     # close connection when done
@@ -98,10 +115,7 @@ if __name__ == '__main__':
     h1.disconnect()
 
     
-    # set global disconnect true to stop new thread
-    disconnect = True
-    log.debug("waiting for message listening thread to finish")
-    t.join()
+
     
 
    
